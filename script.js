a86a8693e71e803f241e119c78628f332e6d834b
Crepiks <sayazhan.onlassyn@mail.ru>
2020-03-26 22:51:42 +0600

    feat: activate appropriate links in scrolling

--- script.js
+++ script.js
@@ -5,6 +5,53 @@ window.onload = function() {
 function activateNavLinks() {
     var NAV_LINKS = document.getElementsByClassName('top-nav__link');
     listenEventOnElements(NAV_LINKS, 'click', handleNavLinkClick);
+    window.addEventListener('scroll', activateNavLinkOnScroll);
+}
+
+function activateNavLinkOnScroll() {
+    var HEADER_HEIGHT = 95;
+    var currentOffset = window.pageYOffset;
+    var sections = getSections();
+    for (var i = 0; i < sections.length; i++) {
+        if (currentOffset >= sections[i].offsetTop - HEADER_HEIGHT - 1 && currentOffset <= sections[i].offsetTop + sections[i].offsetHeight - HEADER_HEIGHT) {
+            var associatedLink = getLinkByAnchor(sections[i].id);
+            if (!linkIsActive(associatedLink)) {
+                inactivateNavLinks();
+                activateNavLink(associatedLink);
+            }
+        }
+    }
+}
+
+function getLinkByAnchor(url) {
+    var NAV_LINKS = document.getElementsByClassName('top-nav__link');
+    for (var i = 0; i < NAV_LINKS.length; i++) {
+        if (getAnchor(NAV_LINKS[i].href) === url) {
+            return NAV_LINKS[i];
+        }
+    }
+    return null;
+}
+
+function linkIsActive(link) {
+    return link.classList.contains('top-nav__link_active');
+}
+
+
+function activateNavLink(link) {
+    link.classList.add('top-nav__link_active');
+}
+
+function getSections() {
+    var NAV_LINKS = document.getElementsByClassName("top-nav__link");
+    var sections = [];
+    for (var i = 0; i < NAV_LINKS.length; i++) {
+        var section = document.getElementById(getAnchor(NAV_LINKS[i].href));
+        if (section) {
+            sections.push(section);
+        }
+    }
+    return sections;
 }
 
 function listenEventOnElements(elements, event, cb) {
